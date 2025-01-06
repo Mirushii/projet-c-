9796f18b9e3947e4828ac891cc7f81f405b8e8e6
Mirushii <147480738+Mirushii@users.noreply.github.com>
2025-01-06 11:24:21 +0100

    Update cheat.h

--- cheat/cheat.h
+++ cheat/cheat.h
@@ -16,16 +16,11 @@ namespace cheat {
     extern bool isInfAmmoOn;
     extern bool isNoRecoilOn;
     extern bool isGetInfoOn;
-    extern bool isInfArmorOn;
-    extern bool isInfArmorOn;
 
     extern std::uintptr_t headPtr;
     extern std::uintptr_t entityL;
     extern std::uintptr_t entity;
     extern float headValue;
-
-    static int initialArmor; // Valeur initiale de l'armure
-    static bool isInfArmorOn; // Indicateur pour l'état de l'armure infinie
 
     void godmodeon() noexcept;
     void godmodeoff() noexcept;
@@ -35,9 +30,6 @@ namespace cheat {
 
     void infammoon() noexcept;
     void infammooff() noexcept;
-
-    void infarmoron() noexcept;
-    void infarmoroff() noexcept;
 
     void norecoilon() noexcept;
     void norecoiloff() noexcept;
