ce476851ca975f660e44370ddad286b8e5487303
Mirushii <147480738+Mirushii@users.noreply.github.com>
2025-01-06 11:25:24 +0100

    Update memory.h

--- cheat/memory.h
+++ cheat/memory.h
@@ -8,16 +8,13 @@
 #include <cstdint>
 #include <string_view>
 
-
-
 class Memory
 {
 private:
 	std::uintptr_t procId = 0;
 	void* processHandle = nullptr;
-public:
 
-	// Memory Constructor - used for finding the processId and opening the handle
+public:
 	Memory(const std::string_view processName) noexcept
 	{
 		::PROCESSENTRY32 entry = { };
@@ -35,19 +32,16 @@ public:
 			}
 		}
 
-		// Close the handle
 		if (procSnap)
 			::CloseHandle(procSnap);
 	}
 
-	// Memory Deconstructor - cleanup by closing the process handle to the game's memory.
 	~Memory()
 	{
 		if (processHandle)
 			::CloseHandle(processHandle);
 	}
 
-	// Get the Module Base Address
 	const std::uintptr_t GetModuleAddress(const std::string_view moduleName) const noexcept
 	{
 		::MODULEENTRY32 entry = {};
@@ -72,7 +66,6 @@ public:
 		return moduleBase;
 	}
 
-	// Read Process Memory
 	template <typename T>
 	constexpr const T Read(const std::uintptr_t& address) const noexcept
 	{
@@ -81,29 +74,26 @@ public:
 		return value;
 	}
 
-	// Write Process Memory
 	template <typename T>
 	constexpr void Write(const std::uintptr_t& address, const T& value) const noexcept
 	{
 		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
 	}
-	// Surcharge pour écrire un tableau de bytes
-	void Write2(const uintptr_t& address, const BYTE* bytes, size_t size) const noexcept {
+
+	void Write2(const uintptr_t& address, const BYTE* bytes, size_t size) const noexcept
+	{
 		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), bytes, size, NULL);
 	}
 
-	// Surcharge pour écrire un std::vector de bytes
-	void Write3(const uintptr_t& address, const std::vector<BYTE>& bytes) const noexcept {
+	void Write3(const uintptr_t& address, const std::vector<BYTE>& bytes) const noexcept
+	{
 		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), bytes.data(), bytes.size(), NULL);
 	}
 
-	// Dans votre classe Memory, ajoutez une méthode pour changer la protection de la mémoire
-	bool ChangeMemoryProtection(const std::uintptr_t& address, SIZE_T size, DWORD newProtect, DWORD& oldProtect) const noexcept {
+	bool ChangeMemoryProtection(const std::uintptr_t& address, SIZE_T size, DWORD newProtect, DWORD& oldProtect) const noexcept
+	{
 		return ::VirtualProtectEx(processHandle, reinterpret_cast<LPVOID>(address), size, newProtect, &oldProtect) != 0;
 	}
-
 };
 
-// Déclaration de la fonction d'accès
 Memory& getMemory();
-
