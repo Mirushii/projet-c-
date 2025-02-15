81f6aae9cfb879143ad7b1edaac20a2389a68224
Mirushii <147480738+Mirushii@users.noreply.github.com>
2025-02-15 15:26:22 +0100

    Update memory.h

--- cheat/memory.h
+++ cheat/memory.h
@@ -11,89 +11,89 @@
 class Memory
 {
 private:
-	std::uintptr_t procId = 0;
-	void* processHandle = nullptr;
+std::uintptr_t procId = 0;
+void* processHandle = nullptr;
 
 public:
-	Memory(const std::string_view processName) noexcept
-	{
-		::PROCESSENTRY32 entry = { };
-		entry.dwSize = sizeof(::PROCESSENTRY32);
+Memory(const std::string_view processName) noexcept
+{
+::PROCESSENTRY32 entry = { };
+entry.dwSize = sizeof(::PROCESSENTRY32);
 
-		const auto procSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+const auto procSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-		while (::Process32Next(procSnap, &entry))
-		{
-			if (!processName.compare(entry.szExeFile))
-			{
-				procId = entry.th32ProcessID;
-				processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
-				break;
-			}
-		}
+while (::Process32Next(procSnap, &entry))
+{
+if (!processName.compare(entry.szExeFile))
+{
+procId = entry.th32ProcessID;
+processHandle = ::OpenProcess(PROCESS_ALL_ACCESS, NULL, procId);
+break;
+}
+}
 
-		if (procSnap)
-			::CloseHandle(procSnap);
-	}
+if (procSnap)
+::CloseHandle(procSnap);
+}
 
-	~Memory()
-	{
-		if (processHandle)
-			::CloseHandle(processHandle);
-	}
+~Memory()
+{
+if (processHandle)
+::CloseHandle(processHandle);
+}
 
-	const std::uintptr_t GetModuleAddress(const std::string_view moduleName) const noexcept
-	{
-		::MODULEENTRY32 entry = {};
-		entry.dwSize = sizeof(::MODULEENTRY32);
+const std::uintptr_t GetModuleAddress(const std::string_view moduleName) const noexcept
+{
+::MODULEENTRY32 entry = {};
+entry.dwSize = sizeof(::MODULEENTRY32);
 
-		const auto procSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
+const auto procSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPMODULE | TH32CS_SNAPMODULE32, procId);
 
-		std::uintptr_t moduleBase = 0;
+std::uintptr_t moduleBase = 0;
 
-		while (::Module32Next(procSnap, &entry))
-		{
-			if (!moduleName.compare(entry.szModule))
-			{
-				moduleBase = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
-				break;
-			}
-		}
+while (::Module32Next(procSnap, &entry))
+{
+if (!moduleName.compare(entry.szModule))
+{
+moduleBase = reinterpret_cast<std::uintptr_t>(entry.modBaseAddr);
+break;
+}
+}
 
-		if (procSnap)
-			::CloseHandle(procSnap);
+if (procSnap)
+::CloseHandle(procSnap);
 
-		return moduleBase;
-	}
+return moduleBase;
+}
 
-	template <typename T>
-	constexpr const T Read(const std::uintptr_t& address) const noexcept
-	{
-		T value = { };
-		::ReadProcessMemory(processHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
-		return value;
-	}
+template <typename T>
+constexpr const T Read(const std::uintptr_t& address) const noexcept
+{
+T value = { };
+::ReadProcessMemory(processHandle, reinterpret_cast<const void*>(address), &value, sizeof(T), NULL);
+return value;
+}
 
-	template <typename T>
-	constexpr void Write(const std::uintptr_t& address, const T& value) const noexcept
-	{
-		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
-	}
+template <typename T>
+constexpr void Write(const std::uintptr_t& address, const T& value) const noexcept
+{
+::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), &value, sizeof(T), NULL);
+}
 
-	void Write2(const uintptr_t& address, const BYTE* bytes, size_t size) const noexcept
-	{
-		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), bytes, size, NULL);
-	}
+void Write2(const uintptr_t& address, const BYTE* bytes, size_t size) const noexcept
+{
+::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), bytes, size, NULL);
+}
 
-	void Write3(const uintptr_t& address, const std::vector<BYTE>& bytes) const noexcept
-	{
-		::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), bytes.data(), bytes.size(), NULL);
-	}
+void Write3(const uintptr_t& address, const std::vector<BYTE>& bytes) const noexcept
+{
+::WriteProcessMemory(processHandle, reinterpret_cast<void*>(address), bytes.data(), bytes.size(), NULL);
+}
 
-	bool ChangeMemoryProtection(const std::uintptr_t& address, SIZE_T size, DWORD newProtect, DWORD& oldProtect) const noexcept
-	{
-		return ::VirtualProtectEx(processHandle, reinterpret_cast<LPVOID>(address), size, newProtect, &oldProtect) != 0;
-	}
+bool ChangeMemoryProtection(const std::uintptr_t& address, SIZE_T size, DWORD newProtect, DWORD& oldProtect) const noexcept
+{
+return ::VirtualProtectEx(processHandle, reinterpret_cast<LPVOID>(address), size, newProtect, &oldProtect) != 0;
+}
 };
 
 Memory& getMemory();
